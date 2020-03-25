4a2af541c1196eaeec897a5ed1c079e674584a0c
Tousif Islam <tousifislampersonal@outlook.com>
2020-03-25 21:40:47 +0530

    Added image uploading feature

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,27 +1,52 @@
 // const fs = require('fs');
 const multer = require('multer'); // multi part form data
-
+const sharp = require('sharp');
 const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
 const AppError = require('./../utils/AppError');
 const factory = require('./handlerFactory');
 
-const multerStorage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, 'public/img/users');
-    },
-    filename: (req, file, cb) => {
-        const ext = file.mimetype.split('/')[1];
+// const multerStorage = multer.diskStorage({
+//     destination: (req, file, cb) => {
+//         cb(null, 'public/img/users');
+//     },
+//     filename: (req, file, cb) => {
+//         const ext = file.mimetype.split('/')[1];
 
-        cb(null, `user-${req.user.id}-${Date.now()}.${ext}`);
+//         cb(null, `user-${req.user.id}-${Date.now()}.${ext}`);
+//     }
+// });
+
+const multerStorage = multer.memoryStorage();
+
+const multerFilter = (req, file, cb) => {
+    if (file.mimetype.startsWith('image')) {
+        cb(null, true);
+    } else {
+        cb(new AppError('Not an image! Please upload an image', 400), false);
     }
+};
+
+const upload = multer({
+    storage: multerStorage,
+    fileFilter: multerFilter
 });
 
-const multerFilter = (req, file, cb) => {};
-
-const upload = multer({ dest: 'public/img/users' });
-
 exports.uploadUserPhoto = upload.single('photo');
+
+exports.resizeUserPhoto = (req, res, next) => {
+    if (!req.file) return next();
+
+    req.file.filename = `user-${req.user.id}-${Date.now()}.jpeg`;
+
+    sharp(req.file.buffer)
+        .resize(500, 500)
+        .toFormat('jpeg')
+        .jpeg({ quality: 85 })
+        .toFile(`public/img/users/${req.file.filename}`);
+
+    next();
+};
 
 const filterObj = (obj, ...allowedFields) => {
     const filteredObj = {};
@@ -55,6 +80,11 @@ exports.updateMe = catchAsync(async (req, res, next) => {
     }
     // update details
     const filteredBody = filterObj(req.body, 'name', 'email');
+    // console.log(req.file);
+    if (req.file) {
+        filteredBody.photo = req.file.filename;
+    }
+
     const updatedUser = await User.findByIdAndUpdate(
         req.user._id,
         filteredBody,

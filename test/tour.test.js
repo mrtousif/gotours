94654f4753aa89e59610237f8b94e8dad89e20a1
Tousif <tousifislampersonal@outlook.com>
2021-01-15 12:35:37 +0530

    tests are updated with better info

--- test/tour.test.js
+++ test/tour.test.js
@@ -2,35 +2,39 @@ const { expect } = require('chai');
 const app = require('../app');
 const request = require('supertest')(app);
 
-describe('GET /api/v1/tours', function () {
-    it('returns all tours in json', async function () {
-        // request(app)
-        //     .get('/api/v1/tours')
-        //     .set('Accept', 'application/json')
-        //     .expect('Content-Type', /json/)
-        //     .expect(200, done);
+describe('TOUR Service', () => {
+    describe('GET /api/v1/tours', function () {
+        it('should return all tours', async function () {
+            // request(app)
+            //     .get('/api/v1/tours')
+            //     .set('Accept', 'application/json')
+            //     .expect('Content-Type', /json/)
+            //     .expect(200, done);
 
-        const res = await request.get('/api/v1/tours');
-        // console.log(res.body);
-        expect(res.status).to.eql(200);
-        // expect(res.body.data.docs.length).to.above(0);
-        expect(res.body.data).to.include.keys('docs');
+            const res = await request.get('/api/v1/tours');
+            // console.log(res.body);
+            expect(res.status).to.eql(200);
+            // expect(res.body.data.docs.length).to.above(0);
+            expect(res.body.data).to.include.keys('docs');
+        });
     });
-});
 
-describe('GET /api/v1/tours/:tourId', function () {
-    it('returns one tours in json', async function () {
-        // request(app)
-        //     .get('/api/v1/tours')
-        //     .set('Accept', 'application/json')
-        //     .expect('Content-Type', /json/)
-        //     .expect(200, done);
+    describe('GET /api/v1/tours/:id', function () {
+        it('should return the tour of given is', async function () {
+            // request(app)
+            //     .get('/api/v1/tours')
+            //     .set('Accept', 'application/json')
+            //     .expect('Content-Type', /json/)
+            //     .expect(200, done);
 
-        const res = await request.get('/api/v1/tours/5c88fa8cf4afda39709c2955');
+            const res = await request.get(
+                '/api/v1/tours/5c88fa8cf4afda39709c2955'
+            );
 
-        // console.log(res.body);
-        expect(res.status).to.eql(200);
-        // expect(res.body.data.docs.length).to.above(0);
-        expect(res.body.data).to.include.keys('doc');
+            // console.log(res.body);
+            expect(res.status).to.eql(200);
+            // expect(res.body.data.docs.length).to.above(0);
+            expect(res.body.data).to.include.keys('doc');
+        });
     });
 });
--- test/user.test.js
+++ test/user.test.js
@@ -26,90 +26,92 @@ before(async () => {
     // console.log(admin);
 });
 
-describe('POST /api/v1/users/signup', function () {
-    it('signs up a user', async function () {
-        const response = await request.post('/api/v1/users/signup').send({
-            name: 'Tousif',
-            email: 'toustif@gotours.com',
-            password: '1234567890',
-            confirmPassword: '1234567890',
+describe('USER Service', () => {
+    describe('POST /api/v1/users/signup', function () {
+        it('sign up a user', async function () {
+            const response = await request.post('/api/v1/users/signup').send({
+                name: 'Tousif',
+                email: 'toustif@gotours.com',
+                password: '1234567890',
+                confirmPassword: '1234567890',
+            });
+
+            expect(response.status).to.eql(201);
+
+            const { body } = response;
+            // console.log(body);
+            expect(body).to.include.keys('token', 'data', 'status');
+            expect(body.token.length).to.above(10);
         });
-
-        expect(response.status).to.eql(201);
-
-        const { body } = response;
-        expect(body).to.include.keys('token', 'data', 'status');
-        expect(body.token.length).to.above(10);
-        expect(body.status).to.eql('success');
     });
-});
 
-describe('POST /api/v1/users/signup', function () {
-    it('tries to sign up a user without confirmPassword', async function () {
-        const response = await request.post('/api/v1/users/signup').send({
-            name: 'Tousif',
-            email: 'toustif@tours.com',
-            password: '1234567890',
-            // confirmPassword: '1234567890',
+    describe('POST /api/v1/users/signup', function () {
+        it('When user try to sign up without confirmPassword, it should return error', async function () {
+            const response = await request.post('/api/v1/users/signup').send({
+                name: 'Tousif',
+                email: 'toustif@tours.com',
+                password: '1234567890',
+                // confirmPassword: '1234567890',
+            });
+            // console.log(response.body);
+            // expect(response.status).to.eql(400);
+
+            const { body } = response;
+            expect(body.status).to.eql('error');
         });
-        // console.log(response.body);
-        // expect(response.status).to.eql(400);
-
-        const { body } = response;
-        expect(body.status).to.eql('error');
     });
-});
 
-describe('POST /api/v1/users/login', function () {
-    it('logs in a user', async function () {
-        const response = await request.post('/api/v1/users/login').send({
-            email: 'toustif@alkon.com',
-            password: '1234567890',
+    describe('POST /api/v1/users/login', function () {
+        it('user log in with valid information', async function () {
+            const response = await request.post('/api/v1/users/login').send({
+                email: 'toustif@alkon.com',
+                password: '1234567890',
+            });
+
+            expect(response.status).to.eql(200);
+
+            const { body } = response;
+            expect(body).to.include.keys('token', 'data', 'status');
+            expect(body.token.length).to.above(10);
+            expect(body.status).to.eql('success');
         });
-
-        expect(response.status).to.eql(200);
-
-        const { body } = response;
-        expect(body).to.include.keys('token', 'data', 'status');
-        expect(body.token.length).to.above(10);
-        expect(body.status).to.eql('success');
     });
-});
 
-describe('POST /api/v1/users/login', function () {
-    it('try to log in a user with a invalid password', async function () {
-        const response = await request.post('/api/v1/users/login').send({
-            email: 'toustif@alkon.com',
-            password: '1234567',
+    describe('POST /api/v1/users/login', function () {
+        it('When user try to log in with a invalid password, should return 401', async function () {
+            const response = await request.post('/api/v1/users/login').send({
+                email: 'toustif@alkon.com',
+                password: '1234567',
+            });
+
+            expect(response.status).to.eql(401);
+
+            const { body } = response;
+            // console.log(body);
+            expect(body).to.include.keys('status', 'message', 'error');
+            // expect(body.token.length).to.above(10);
+            expect(body.message).to.eql('Incorrect email or password');
         });
-
-        expect(response.status).to.eql(401);
-
-        const { body } = response;
-        // console.log(body);
-        expect(body).to.include.keys('status', 'message', 'error');
-        // expect(body.token.length).to.above(10);
-        expect(body.message).to.eql('Incorrect email or password');
     });
-});
 
-describe('GET /api/v1/users/:id', function () {
-    it('should require login to get access', async function () {
-        const response = await request.get(
-            '/api/v1/users/5c8a1f292f8fb814b56fa184'
-        );
-        const { body } = response;
+    describe('GET /api/v1/users/:id', function () {
+        it('should require login to get access', async function () {
+            const response = await request.get(
+                '/api/v1/users/5c8a1f292f8fb814b56fa184'
+            );
+            const { body } = response;
 
-        expect(response.status).to.equal(401);
-        expect(body.message).to.eql('You need to login to get access');
+            expect(response.status).to.equal(401);
+            expect(body.message).to.eql('You need to login to get access');
+        });
     });
-});
 
-describe('GET /api/v1/users', function () {
-    it('requires admin role to access the route', async function () {
-        const response = await request.get('/api/v1/users');
-        // console.log(response.body);
+    describe('GET /api/v1/users', function () {
+        it('require admin role to access the route', async function () {
+            const response = await request.get('/api/v1/users');
+            // console.log(response.body);
 
-        expect(response.status).to.oneOf([403, 401]);
+            expect(response.status).to.oneOf([403, 401]);
+        });
     });
 });
